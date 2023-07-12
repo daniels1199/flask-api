1ce0ee5b617e379042e72510eda9f1e40e6a2dd0
Daniel Soares <ds507556@gmail.com>
2023-07-12 00:56:42 -0300

    updating some files

--- app.py
+++ app.py
@@ -14,8 +14,7 @@ class Users(db.Model):
     username = db.Column(db.String(100))
     password = db.Column(db.String(100))
 
-    def __init__(self, username, password):
-            
+    def __init__(self, username, password):     
         self.username = username
         self.password = password
         
